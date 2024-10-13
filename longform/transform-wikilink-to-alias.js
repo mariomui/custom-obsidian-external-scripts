ae7639238a0e477a66b62259ec2725df0fe3fc44
mariomui <yi.kan.mui@gmail.com>
2024-10-12 18:30:52 -0700

    Extend longform export with addon to convert wikilinks to remove everything but the alias

---
+++ longform/transform-wikilink-to-alias.js
@@ -0,0 +1,33 @@
+// author: @mariomui
+//──────────────────────────────────────────────────────────────────────────────
+
+module.exports = {
+  description: {
+    name: 'Strip Wikilink To Alias',
+    description: 'ᛞ--path|alias -> alias',
+    availableKinds: ['Manuscript', 'Scene'],
+    options: [],
+  },
+
+  /**
+   * @param {{ contents: string; }} input
+   * @param {{ app: { metadataCache: { getFirstLinkpathDest: (arg0: any, arg1: any) => { (): any; new (): any; path: any; }; }; }; projectPath: any; }} context
+   */
+  compile(input, context) {
+    return input.map((i) => {
+      const imageWikiLinkRegex = /\!{0,1}\[\[(.*?)(?:\|(.+))?\]\]/g; // https://regex101.com/r/8Qzbod/1
+      const contents = i.contents?.replace(
+        imageWikiLinkRegex,
+        function (_fullmatch, ...args) {
+          const alias = args.at(1);
+          const target = args.at(0).split('#').first();
+          // if (target.includes('--')) {
+          //   return target.split('--').at(1);
+          // }
+          return alias || target;
+        }
+      );
+      return { ...i, contents };
+    });
+  },
+};
